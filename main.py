5f336550e7e77158937277977012bfdf7fb364de
BadVoice <smurovnemez@mail.ru>
2023-10-02 17:01:31 +0300

    feat: questions yes or not

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from dotenv import load_dotenv
 import os
-from aiogram import Bot, types, Dispatcher
-from aiogram.filters import Filter
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types, F
 import asyncio
 
-load_dotenv()
+from handlers import questions, different_types
 
+load_dotenv()
 BOT_ID = os.getenv('BOT_KEY')
 
-bot = Bot(token=BOT_ID)
-dp = Dispatcher()
+# Запуск бота
+async def main():
+    bot = Bot(token=BOT_ID, parse_mode="HTML")
+    dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def my_env_bot_id(message: types.Message):
-    await message.answer(BOT_ID)
+    dp.include_routers(questions.router, different_types.router)
 
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(BOT_ID)
-    # And the run events dispatching
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
-
 
 if __name__ == "__main__":
     asyncio.run(main())
